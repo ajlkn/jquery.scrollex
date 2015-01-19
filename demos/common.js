e5efc0fd630d2411f84b551ffd5d3531fe5ebdf9
aj <aj@n33.co>
2015-01-18 19:04:16 -0600

    Added demos

---
+++ demos/common.js
@@ -0,0 +1,19 @@
+(function($) {
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 0);
+			});
+
+	});
+
+})(jQuery);
